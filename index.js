d72936d5193fe420c20e11be8d0883671333b6e8
Sayuri Kamble <>
2021-05-21 04:39:13 +0000

    API for Quiz 1 created

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 
+
+
 const app = express();
+
+
+const mongoose = require("mongoose")
+require("dotenv")
+const bodyParser = require('body-parser')
+const cors = require("cors")
+
+app.use(bodyParser.json())
+app.use(cors())
+
+
+const quiz1route = require("./Routes/quiz1.route")
 
 app.get('/', (req, res) => {
   res.send('Hello Express app!')
@@ -9,3 +23,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('server started');
 });
+
+app.use("/quiz1", quiz1route)
